456239e41d949ac5d754938c37bd9d00645f058c
jade.kjo <jade.kjo@kakaocorp.com>
2022-01-04 00:57:27 +0900

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+
+//여기도 추가
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/app.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true,
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/app.html",
+      title: "Development",
+    }),
+  ],
+};
